bc662e2d4c7b41a0ec24c618b5f79d06a99b4fbb
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-05-31 11:32:05 +0200

    Updated my files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import React from "react";
+import Header from "./components/header" 
+import Meme from "./components/Meme"; 
+/*import Meme from "Meme";*/
 
 function App() {
 
   return (
     <>
-     
+     <Header/>
+     <Meme/>
     </>
-  )
+  );
 }
-
 export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+/*import Header from "./components/Header"*/
+/**
+ * Challenge: Build the Header component
+ */
+ function Header() {
+    return (
+        <header calssName = "header">
+            img src = ".image"
+        <h1>Header Component</h1>
+        <h4>React Course Projecst 3</h4>
+        </header>
+     
+        
+    )
+}
